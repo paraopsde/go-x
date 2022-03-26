a7396782b812b645ca21c499cf3d3046a84c3812
Tobias Hintze <th-git@thzn.de>
2022-03-26 09:56:19 +0100

    add pkg/util/
    - for now just some logging

---
+++ pkg/util/log.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type contextKey string
+
+func (c contextKey) String() string {
+	return "context key " + string(c)
+}
+
+func NewLogger() *zap.Logger {
+	return NewLoggerWithLevel(zap.DebugLevel)
+}
+func NewLoggerWithLevel(level zapcore.Level) *zap.Logger {
+	conf := zap.NewProductionConfig()
+	conf.Level.SetLevel(level)
+	log, err := conf.Build()
+	if err != nil {
+		panic("failed to create logger")
+	}
+	return log.WithOptions(zap.WithCaller(true), zap.AddStacktrace(zap.WarnLevel))
+}
+
+func CtxLogOrPanic(ctx context.Context) *zap.Logger {
+	log, ok := ctx.Value(contextKey("logger")).(*zap.Logger)
+	if !ok {
+		panic("context lacks logger")
+	}
+	return log
+}
+
+func CtxWithLog(ctx context.Context, log *zap.Logger) context.Context {
+	return context.WithValue(ctx, contextKey("logger"), log)
+}
